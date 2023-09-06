2f359ee1ea7f2a4a304962300c44dfac645374d3
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-06 18:18:00 -0400

    created sign up page

---
+++ my-app/src/app/auth/signUp/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useState, FormEvent } from "react"
+import { signIn } from "next-auth/react"
+
+export default function SignIn() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // signIn("credentials", { username: username, password: password, callbackUrl: "http://localhost:3000" })
+  }
+
+  return(
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-200">
+      <form className="flex flex-col w-auto h-auto bg-slate-300 p-6 gap-2 rounded-lg" onSubmit={onSubmit}>
+        <h1 className="font-bold">Create account</h1>
+        <label className="ml-2.5">Username: </label>
+        <input type="text" placeholder="Username" className="p-2.5 rounded-lg" onChange={(e) => {setUsername(e.target.value)}}/>
+        <label className="ml-2.5">Password: </label>
+        <input type="password" placeholder="Password" className="p-2.5 rounded-lg" onChange={(e) => {setPassword(e.target.value)}}/>
+        <button className="bg-slate-600 hover:bg-slate-500 p-4 mt-2 rounded-lg font-bold text-slate-100">Create Account</button>
+      </form>
+    </div>
+  )
+}
