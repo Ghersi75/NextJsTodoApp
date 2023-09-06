284dc5be3133728a2c3847d0e3b1b4afab1fb664
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-06 18:16:46 -0400

    removed required login on home page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -13,12 +13,14 @@ import { redirect } from "next/navigation"
 
 export default function Home() {
   // Require login and send to any url if not authenticated
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect("api/auth/signin")
-    }
-  })
+  const { data: session } = useSession(
+    // {
+    // required: true,
+    // onUnauthenticated() {
+    //   redirect("/auth/signin")
+    // }
+  // }
+  )
   console.log(session)
   const [darkMode, setDarkMode] = useAtom(darkModeAtom)
   // const [session, setSession] = useState<Session | null>()
@@ -29,7 +31,7 @@ export default function Home() {
         <h1 className="text-2xl text-bold">Todo List App</h1>
         {
           session?.user ? 
-          <h1> Logged in </h1> :
+          <h1> Logged in as {session?.user?.name}</h1> :
           <h1> Nope </h1>
         }
         <AddTask />
