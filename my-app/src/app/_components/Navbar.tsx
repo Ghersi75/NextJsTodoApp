c32671fe5d591da1295cac855fa3beb15f49550b
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-06 18:17:02 -0400

    Switched to client and added signOut button

--- my-app/src/app/_components/Navbar.tsx
+++ my-app/src/app/_components/Navbar.tsx
@@ -1,7 +1,21 @@
+"use client"
+
+import { signOut } from "next-auth/react"
+import { usePathname } from "next/navigation"
+import { useSession } from "next-auth/react"
+// import { useState } from "react"
+
 export default function Navbar() {
+  const pathname = usePathname()
+  const { data: session } = useSession()
+  // const [showBar, setShowBar] = useState(!["/auth/signIn", "/auth/signUp"].includes(pathname))
+  console.log(pathname)
   return(
-    <nav className="fixed top-0 left-0 w-screen z-20 bg-slate-500 h-16">
-      Things
-    </nav>
+    <>
+      <nav className="fixed top-0 left-0 w-screen z-20 bg-slate-500 h-16">
+        Things
+        {session?.user && pathname !== "/auth/signIn" && pathname !== "/auth/signUp" && <button className="p-4 bg-slate-200" onClick={() => {signOut()}}>Sign Out</button>}
+      </nav> 
+    </>
   )
 }
