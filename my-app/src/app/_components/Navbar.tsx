22f042fcd097e3b221d8f242df072ce34c507e78
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-08 15:03:50 -0400

    Made styling changes to navbar

--- my-app/src/app/_components/Navbar.tsx
+++ my-app/src/app/_components/Navbar.tsx
@@ -9,12 +9,11 @@ export default function Navbar() {
   const pathname = usePathname()
   const { data: session } = useSession()
   // const [showBar, setShowBar] = useState(!["/auth/signIn", "/auth/signUp"].includes(pathname))
-  console.log(pathname)
+  // console.log(pathname)
   return(
     <>
-      <nav className="fixed top-0 left-0 w-screen z-20 bg-slate-500 h-16">
-        Things
-        {session?.user && pathname !== "/auth/signIn" && pathname !== "/auth/signUp" && <button className="p-4 bg-slate-200" onClick={() => {signOut()}}>Sign Out</button>}
+      <nav className="fixed top-0 left-0 w-screen z-20 bg-slate-500 h-16 p-2.5 flex items-center justify-center">
+        {session?.user && pathname !== "/auth/signIn" && pathname !== "/auth/signUp" && <button className="p-2.5 bg-slate-200 rounded-lg" onClick={() => {signOut()}}>Sign Out</button>}
       </nav> 
     </>
   )
