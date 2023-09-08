fc4db46fea00b9ff1dced3549d2534b5d6740769
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-08 15:05:11 -0400

    Added redirect on login

--- my-app/src/app/auth/signIn/page.tsx
+++ my-app/src/app/auth/signIn/page.tsx
@@ -2,10 +2,12 @@
 
 import { useState, FormEvent } from "react"
 import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 export default function SignIn() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const router = useRouter()
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
   //   e.preventDefault()
@@ -16,8 +18,10 @@ export default function SignIn() {
 
   const onClick = async (e: any) => {
       e.preventDefault()
-      const res = await signIn("credentials", { username: "user", password: password, redirect: false })
-      console.log(res)
+      const res = await signIn("credentials", { username: username, password: password, redirect: false })
+      if (res?.ok) {
+        router.push("/")
+      }
   }
 
 
@@ -30,6 +34,7 @@ export default function SignIn() {
         <label className="ml-2.5">Password: </label>
         <input type="password" placeholder="Password" className="p-2.5 rounded-lg" onChange={(e) => {setPassword(e.target.value)}}/>
         <button className="bg-slate-600 hover:bg-slate-500 p-4 mt-2 rounded-lg font-bold text-slate-100" onClick={onClick}>Sign In</button>
+        <span>Don't have an account? <a className="cursor-pointer" onClick={() => {router.push("/auth/signUp")}}>Sign Up</a></span>
       </form>
     </div>
   )
