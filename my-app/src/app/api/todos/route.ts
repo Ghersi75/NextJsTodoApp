26fe626540dddf934b328af8efa692259bb3a0d2
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-08 19:33:36 -0400

    added user based todo fetching

--- my-app/src/app/api/todos/route.ts
+++ my-app/src/app/api/todos/route.ts
@@ -1,11 +1,43 @@
 import { db } from "@/utils/database"
+import { NextResponse } from 'next/server'
+import type { NextApiRequest } from 'next'
 
-export async function GET(request: Request) {
+export async function GET(req: NextApiRequest) {
+  const url = new URL(req.url as string).searchParams
+  const user_id: number | null = Number(url.get("user_id"))
+  const username: string | null = url.get("username")
+  
+  if (!user_id && !username) {
+    return new NextResponse(JSON.stringify({
+      error: "No user_id or username provided"
+    }), {
+      status: 400
+    })
+  }
+
   try {
-    const todos = await db.selectFrom("todo").selectAll().execute()
+    let todos
+    if (user_id) {
+      todos = await db.selectFrom("todo").where("user_id", "=", user_id).selectAll().execute()
+    } else if (username) {
+      const user = await db.selectFrom("user").select("user_id").where("username", "=", username).executeTakeFirst()
+      if (!user?.user_id) {
+        return new NextResponse(JSON.stringify({
+          error: "No user with given username found"
+        }), {
+          status: 400
+        })
+      }
+
+      todos = await db.selectFrom("todo").selectAll().where("user_id", "=", user.user_id).execute()
+    }
     
     // console.log(todos);
-    return new Response(JSON.stringify(todos));
+    return new NextResponse(JSON.stringify({
+      todos
+    }), {
+      status: 200
+    })
   } catch (error) {
     console.error("Error executing query:", error);
     return new Response("Server error", { status: 500 });
@@ -14,25 +46,40 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-  const data = await request.json()
-  if (!data.description) {
-    return new Response(JSON.stringify({ message: "Bad request, description not found" }), {
+  const { user, todo } = await request.json()
+  if (!user || !todo) {
+    return new NextResponse(JSON.stringify(
+      { 
+        error: "User or new Todo missing" 
+      }), {
       status: 400,
     })
   }
 
+  // console.log(user, todo)
 
   try {
+    const foundUser = await db.selectFrom("user").select("user_id").where("email", "=", user.email).executeTakeFirst()
+    if (!foundUser?.user_id) {
+      return new NextResponse(JSON.stringify({
+        error: "User with given email does not exist"
+      }), {
+        status: 400
+      })
+    }
+
     const res = await db.insertInto("todo")
       .values({
-        description: data.description,
-        is_completed: false // default value
+        description: todo.description,
+        is_completed: false, // default value
+        user_id: foundUser.user_id
       })
       .executeTakeFirst()
 
-    return new Response(JSON.stringify({
+    return new NextResponse(JSON.stringify({
       todo_id: Number(res.insertId), //bigInt to number
-      description: data.description,
+      user_id: foundUser.user_id,
+      description: todo.description,
       is_completed: false
     }), {
       status: 200
